71024909509ba4fa7219caaaaeb55940810bd379
23simon <simondasilvavilas@gmail.com>
2021-11-20 22:38:23 +0100

    Recogida de tabla json, pasado a array / Cambio de nombre de los ficheros

---
+++ views/formulario.view.php
@@ -0,0 +1,53 @@
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800"><?php echo $data['titulo']; ?></h1>
+
+</div>
+
+<!-- Content Row -->
+
+<div class="row">
+    <?php
+    if (isset($data['post'])) {
+        ?>
+        <div class="col-12">
+            <div class="card shadow mb-4">
+                <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Variables pasadas:</h6>                                    
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <?php var_dump($data['post']); ?>
+                    <?php var_dump($data['sanitized_post']); ?>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="col-12">
+        <div class="card shadow mb-4">
+            <div
+                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo']; ?></h6>                                    
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <form action="./?sec=formulario" method="post">
+                    <!--<form method="get">-->
+                    <input type="hidden" name="sec" value="formulario" />
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1">Introducir texto</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" name="textarea" rows="3"><?php echo isset($data['sanitized_post']['textarea']) ? $data['sanitized_post']['textarea'] : ''; ?></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" value="Enviar" name="submit" class="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>                        
+</div>
+
