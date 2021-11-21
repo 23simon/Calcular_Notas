5af6970733a8c495e2d6c6613c6385cdb3ed46b1
23simon <simondasilvavilas@gmail.com>
2021-11-21 17:59:35 +0100

    Reparación de array / Array resultado construido

--- controllers/formulario.simon.controller.php
+++ controllers/formulario.simon.controller.php
@@ -14,12 +14,62 @@ if(isset($_POST['submit'])){
     $array = json_decode($_POST['textarea']);
     $resultado=array();
     foreach ($array as $materia => $valor){
-        $resultado[]=$materia;
-        foreach ($valor as $alumno => $nota){
-            $resultado[$materia][]=$alumno;
-            $resultado[$materia][$alumno]= $nota;
+        foreach ($valor as $alumno => $notas){
+            $media=0;
+            $contador=0;
+            foreach ($notas as $nota){
+                $media+=$nota;
+                $contador++;
+            }
+            $resultado[$materia][$alumno]= round(($media/$contador) , 2);
         }
     }
+    var_dump($resultado=arrayFinal($resultado));
+}
+
+function arrayFinal($resultado){
+    foreach ($resultado as $materia => $alumnos){
+        $media=0;
+        $suspenso=0;
+        $aprobado=0;
+        $contador=0;
+        $alumnoMin="";
+        $minima=10;
+        $alumnoMax="";
+        $maxima=0;
+        $_resultado[]=$materia;
+        
+        foreach ($resultado[$materia] as $alumno => $nota){
+            $media+= $nota;
+            $contador++;
+            $nota=$nota;
+            if($nota<5){
+                $suspenso++;
+            }else{
+                $aprobado++;
+            }
+            
+            if($nota>$maxima){
+                $maxima = $nota;
+                $alumnoMax=$alumno;
+            }
+            if($nota<$minima){
+                $minima=$nota;
+                $alumnoMin=$alumno;
+            }
+        }
+        $media=$media/$contador;
+        
+        $_resultado[$materia]['media']= $media;
+        $_resultado[$materia]["suspensos"]= $suspenso;
+        $_resultado[$materia]["aprobados"]= $aprobado;
+        $_resultado[$materia]["max"]["alumno"]=$alumnoMax;
+        $_resultado[$materia]["max"]["nota"]=$maxima;
+        $_resultado[$materia]["min"]["alumno"]=$alumnoMin;
+        $_resultado[$materia]["min"]["nota"]=$minima;
+    }
+    
+    return $_resultado;
 }
 
 include 'views/templates/header.php';
